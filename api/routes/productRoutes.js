d0241e01e7d5c7fc4e2c3bcf59ac81a62b4b3016
Shahbaazkyz <shahbaazkyz@gmail.com>
2022-01-29 21:10:00 +0500

    ✅ Get single Product!

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -13,5 +13,22 @@ router.get(
     res.json(products);
   })
 );
+// * @desc    Get Single product
+// * @route   GET api/product
+// * @access  Public
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      return res.status(200).json(product);
+    } else {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+  })
+);
 
 module.exports = router;
