f9c33dab17353f8695943858d1e120d05d81cfe0
Shahbaazkyz <shahbaazkyz@gmail.com>
2022-01-29 23:10:00 +0500

    ✅ Error handlers added!

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -24,9 +24,8 @@ router.get(
     if (product) {
       return res.status(200).json(product);
     } else {
-      return res.status(404).json({
-        message: "Product not found",
-      });
+      res.status(404);
+      throw new Error("Product not Found");
     }
   })
 );
--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 const dotenv = require("dotenv");
 
@@ -10,5 +11,8 @@ connectDB();
 const port = process.env.PORT;
 
 app.use("/api/products", require("./routes/productRoutes"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
