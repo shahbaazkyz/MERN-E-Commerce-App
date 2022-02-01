cba4fe9f183af8fe6f1ff7b2f4e4d48af33bdcc6
Shahbaazkyz <shahbaazkyz@gmail.com>
2022-02-01 20:10:00 +0500

    ✅ Cart Screen added!

--- frontend/src/pages/CartScreen.js
+++ frontend/src/pages/CartScreen.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams, useSearchParams } from "react-router-dom";
+import {
+  Link,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 import Message from "../components/Message";
 import {
   Row,
@@ -11,12 +16,13 @@ import {
   ListGroup,
   Form,
 } from "react-bootstrap";
-import { addToCart } from "../redux/actions/cartActions";
+import { addToCart, removeFromCart } from "../redux/actions/cartActions";
 import { useLocation } from "react-router-dom";
 
 const CartScreen = () => {
   const { id } = useParams();
   const { search } = useLocation();
+  const navigate = useNavigate();
   const qty = search ? Number(search.split("=")[1]) : 1;
   console.log(qty);
 
@@ -31,7 +37,102 @@ const CartScreen = () => {
     }
   }, [dispatch, id, qty]);
 
-  return <div>Cart Screen</div>;
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const checkoutHandler = () => {
+    navigate("/login?redirect=shipping");
+  };
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Cart is empty <Link to="/"> Go Back. </Link>{" "}
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>Rs. {item.price}</Col>
+
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+
+                  <Col md={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h4>
+                Sub Total (
+                {cartItems.reduce((acc, currItem) => acc + currItem.qty, 0)})
+                items
+              </h4>
+              Rs.{" "}
+              {cartItems
+                .reduce(
+                  (acc, currItem) => acc + currItem.price * currItem.qty,
+                  0
+                )
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                variant="primary"
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Proceed to Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default CartScreen;
