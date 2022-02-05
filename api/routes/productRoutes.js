f6cfa66850153bbfa6fd3427a279ef94fe37e8e8
Shahbaazkyz <shahbaazkyz@gmail.com>
2022-02-05 19:16:38 +0500

    ✅ Product Controller Added!

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,33 +1,13 @@
 const router = require("express").Router();
-const asyncHandler = require("express-async-handler");
-const Product = require("../models/productModel");
+const {
+  getSingleProduct,
+  getAllProducts,
+} = require("../controllers/productController");
 
-// * @desc    Get all products
-// * @route   GET api/products
-// * @access  Public
+//* Method 1
+router.route("/").get(getAllProducts);
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find();
-    res.json(products);
-  })
-);
-// * @desc    Get Single product
-// * @route   GET api/product
-// * @access  Public
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.status(200).json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not Found");
-    }
-  })
-);
+//* Method 2
+router.get("/:id", getSingleProduct);
 
 module.exports = router;
