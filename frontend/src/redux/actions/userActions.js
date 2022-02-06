5c5ed5cc3d02614c6dff015506f09648e21b3cf5
Shahbaazkyz <shahbaazkyz@gmail.com>
2022-02-07 02:00:00 +0500

    ✅ Logout action added!

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -2,6 +2,7 @@ import {
   USER_LOGIN_FAILURE,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from "../constants/userConstants";
 import axios from "axios";
 
@@ -34,4 +35,10 @@ export const login = (email, password) => async (dispatch) => {
           : error.message,
     });
   }
+};
+
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem("userInfo");
+
+  dispatch({ type: USER_LOGOUT });
 };
